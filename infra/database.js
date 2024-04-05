ef3147b9be576a4bcca35ea29492904b2f8b2bf0
ricassio costa <ricassiocosta.me@gmail.com>
2024-04-05 00:04:16 -0300

    returning database information on `/status`

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,16 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+
+  let result
+  try {
+    await client.connect();
+    result = await client.query(queryObject);
+  } catch(err) {
+    console.error(err)
+  } finally {
+    await client.end()
+  }
 
   return result;
 }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,25 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString()
 
+  const dbVersion = await database.query("SHOW server_version;")
+  const dbVersionValue = dbVersion.rows[0].server_version
+
+  const dbMaxConns = await database.query("SHOW max_connections;")
+  const dbMaxConnsValue = parseInt(dbMaxConns.rows[0].max_connections)
+
+  const dbOpenConns = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+  const dbOpenConnsValue = dbOpenConns.rows[0].count
+  console.log(dbOpenConnsValue)
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnsValue,
+        open_connections: dbOpenConnsValue,
+      }
+    }
   });
 }
 
