b5dfb1a6d3f640160a87600775811e5b24d3212a
ricassio costa <ricassiocosta.me@gmail.com>
2025-10-11 13:07:15 -0300

    finishing day 23

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+async function migrations(request, response) {
+  if (request.method == "GET") {
+    console.log(request.method);
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    return response.status(200).json(migrations);
+  }
+
+  if (request.method == "POST") {
+    console.log(request.method);
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    return response.status(200).json(migrations);
+  }
+
+  return response.status(405).end();
+}
+
+export default migrations;
