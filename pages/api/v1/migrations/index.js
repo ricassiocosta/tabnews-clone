fb792cdf2d39e85133d0f40eafcb5eb8ce6bb57a
ricassio costa <ricassiocosta.me@gmail.com>
2025-10-15 20:53:23 -0300

    feat: testing `POST /migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,32 +2,32 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 async function migrations(request, response) {
-  if (request.method == "GET") {
-    console.log(request.method);
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
-    return response.status(200).json(migrations);
+  if (request.method == "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method == "POST") {
-    console.log(request.method);
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
 
-    return response.status(200).json(migrations);
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
