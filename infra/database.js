da9876c157f74409d01b34dee284c06abaf09c92
ricassio costa <ricassiocosta.me@gmail.com>
2025-10-18 13:22:54 -0300

    feat: extracting pg client creation

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,11 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV == "production" ? true : false,
-  });
-
+  let client;
   let result;
+
   try {
-    await client.connect();
+    client = await getNewClient();
     result = await client.query(queryObject);
   } catch (err) {
     console.error(err);
@@ -24,6 +17,21 @@ async function query(queryObject) {
   return result;
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV == "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
