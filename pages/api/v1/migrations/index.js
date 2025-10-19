5496a671193e2998519141d772e5ef867ebb874a
ricassio costa <ricassiocosta.me@gmail.com>
2025-10-19 14:30:30 -0300

    fix: avoing pending database connection on `migrations` API

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,7 +2,15 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
 
+const allowedMethods = ["GET", "POST"];
+
 async function migrations(request, response) {
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
   const dbClient = await database.getNewClient();
 
   const defaultMigrationOptions = {
@@ -14,28 +22,30 @@ async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  if (request.method == "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method == "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  try {
+    if (request.method == "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method == "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
 
-  return response.status(405).end();
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ message: "internal server error" });
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
