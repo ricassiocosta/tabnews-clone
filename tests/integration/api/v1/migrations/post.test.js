fba8e637893badb4a622e3e47be853fd5e4cfe91
ricassio costa <ricassiocosta.me@gmail.com>
2025-10-28 20:22:09 -0300

    feat: make `npm test` more robust with retries

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator.js"
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices()
+})
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
